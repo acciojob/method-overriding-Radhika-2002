472dc08553d242ac21938ca4c58ce71264fa468c
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-02-15 18:57:30 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,7 +12,7 @@ public class Main {
         // Task 4: Overriding method in class B
         @Override
         public String meth() {
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
 
