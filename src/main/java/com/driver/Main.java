dea875781b74192fde8858b9e1937e2a62a9d69a
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-02-15 18:18:56 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    static class A {
+        // Task 1: Method in class A
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A {
+        // Task 4: Overriding method in class B
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B objB = new B();
+
+        // Task 3: Call method from class A using object of class B
+        System.out.println(objB.meth()); // Output: Method is overridden in Extended class B
+    }
 }
